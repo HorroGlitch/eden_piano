354460dc09a00eaf1eb4288a25dc99d840ac8cae
HorroGlitch <lverot@edenschool.fr>
2024-04-11 17:06:18 +0200

    variable piano octave

--- main.js
+++ main.js
@@ -1,1 +1,47 @@
 import './style.css'
+
+let allTheTiles = document.querySelector(".allTheTiles");
+
+const octavePattern="bnbnbbnbnbnb"
+let octaves_count = 2;
+
+
+
+for (let i = 0; i < octaves_count; i++) {
+
+  for (let j = 0; j < octavePattern.length; j++) {
+
+    if (octavePattern[j] + octavePattern[j+1] === "bn") {
+
+      console.log(octavePattern[j])
+      let whiteTile = document.createElement("div");
+      whiteTile.classList.add("white");
+      allTheTiles.appendChild(whiteTile);
+    
+      let blackTile = document.createElement("div");
+      blackTile.classList.add("black");
+      whiteTile.appendChild(blackTile);
+      
+    
+    } else if(octavePattern[j] + octavePattern[j+1] === "bb"){
+
+      let whiteTile = document.createElement("div");
+      whiteTile.classList.add("white");
+      allTheTiles.appendChild(whiteTile);
+
+    }
+     
+  }
+  let whiteTile = document.createElement("div");
+      whiteTile.classList.add("white");
+      allTheTiles.appendChild(whiteTile);
+  
+}
+let whiteTile = document.createElement("div");
+whiteTile.classList.add("white");
+allTheTiles.appendChild(whiteTile);
+
+  
+
+  
+ 
